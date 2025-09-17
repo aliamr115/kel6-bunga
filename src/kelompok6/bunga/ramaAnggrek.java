4e1fdaac1d0db9b2256435d21b63f4f518fa5d26
ubaydillah <m.ubaiydillah.r@gmail.com>
2025-09-17 18:13:57 +0700

    firstCommit

--- src/kelompok6/bunga/ramaAnggrek.java
+++ src/kelompok6/bunga/ramaAnggrek.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package kelompok6.bunga;
+ package kelompok6.bunga;
 
-/**
- *
- * @author user
- */
 public class ramaAnggrek {
+    // Deklarasi variabel
+    String nama, warna, jenis, harga;
     
+    // Method Setter
+    public void setnama(String namax){
+        this.nama = namax;
+    }
+    
+    public void setwarna(String warnax){
+        this.warna = warnax;
+    }
+    
+    public void setjenis(String jenisx){
+        this.jenis = jenisx;
+    }
+    
+    public void setharga(String hargax){
+        this.harga = hargax;
+    }   
+    
+    // Method Getter
+    public String getnama(){
+        return this.nama;
+    }
+    
+    public String getwarna(){
+        return this.warna;
+    }
+    
+    public String getjenis(){
+        return this.jenis;
+    }
+    
+    public String getharga(){
+        return this.harga;
+    }
 }
