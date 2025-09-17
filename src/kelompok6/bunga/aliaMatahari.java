9c27c03f5c64e6d86af682d73fadde1761cb3b67
aliamr <aliamasrukha@gmail.com>
2025-09-17 18:13:01 +0700

    set-getAlia

--- src/kelompok6/bunga/aliaMatahari.java
+++ src/kelompok6/bunga/aliaMatahari.java
@@ -9,5 +9,35 @@ package kelompok6.bunga;
  * @author user
  */
 public class aliaMatahari {
+    String nama, warna, jenis, harga;
     
+    public void setnama(String nama){ //method setter
+        this.nama = nama;
+    }
+     public String getnama(){ //method getter
+        return this.nama;
+    }
+     
+    public void setwarna(String warna){ //method setter
+        this.warna = warna;
+    }
+     public String getwarna(){ //method getter
+        return this.warna;
+    }
+     
+    public void setjenis(String jenis){ //method setter
+        this.jenis = jenis;
+    }
+     public String getjenis(){ //method getter
+        return this.jenis;
+    }
+     
+    public void setharga(String harga){ //method setter
+        this.harga = harga;
+    }
+     public String getharga(){ //method getter
+        return this.harga;
+    }   
 }
+
+
