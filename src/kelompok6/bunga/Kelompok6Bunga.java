67a365ea78ab8f490776a5d159ef4d7ed702129e
MUHAMMAD MUZAKKY <zakkysaja990@gmail.com>
2025-09-23 16:05:48 +0700

    Selamat Datang

--- src/kelompok6/bunga/Kelompok6Bunga.java
+++ src/kelompok6/bunga/Kelompok6Bunga.java
@@ -4,17 +4,17 @@ package kelompok6.bunga;
 public class Kelompok6Bunga {
     public static void main(String[] args) {
         // TODO code application logic here
-        zakkyTuri pp = new zakkyTuri();
+        zakkyTuri zt = new zakkyTuri();
         
-        pp.setNama("Kembang Turi");
-        pp.setWarna("Putih, Pink, Merah");
-        pp.setJenis("Sesbania grandiflora");
-        pp.setHarga("Rp.8000 per kg");
+        zt.setNama("Kembang Turi");
+        zt.setWarna("Putih, Pink, Merah");
+        zt.setJenis("Sesbania grandiflora");
+        zt.setHarga("Rp.8000 per kg");
         
-        System.out.println("Nama : "+pp.getNama());
-        System.out.println("Jenis : "+pp.getJenis());
-        System.out.println("Warna : "+pp.getWarna());
-        System.out.println("Harga : "+pp.getHarga());
+        System.out.println("Nama : "+zt.getNama());
+        System.out.println("Jenis : "+zt.getJenis());
+        System.out.println("Warna : "+zt.getWarna());
+        System.out.println("Harga : "+zt.getHarga());
     }
     
 }
