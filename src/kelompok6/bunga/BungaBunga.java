06e5cde34ebb67a23a42261ac5f6f6d655723c36
aliamr <aliamasrukha@gmail.com>
2025-10-07 15:39:23 +0700

    inheritance

--- src/kelompok6/bunga/BungaBunga.java
+++ src/kelompok6/bunga/BungaBunga.java
@@ -10,4 +10,44 @@ package kelompok6.bunga;
  */
 public class BungaBunga {
     
+    
+    String warna,ciri_ciri, kelopak;
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getCiri_ciri() {
+        return ciri_ciri;
+    }
+
+    public void setCiri_ciri(String ciri_ciri) {
+        this.ciri_ciri = ciri_ciri;
+    }
+
+    public String getKelopak() {
+        return kelopak;
+    }
+
+    public void setKelopak(String kelopak) {
+        this.kelopak = kelopak;
+    }
+    
+   
+        public void TampilData(){
+              
+              System.out.println("Warna Bunga :" +warna);
+              System.out.println("Ciri Bunga :" +ciri_ciri);
+              System.out.println("Kelopak Bunga :" +kelopak);
+
+            
+    }
 }
+           
+                
+    
+
---
+++ src/kelompok6/bunga/mainClass.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kelompok6.bunga;
+
+/**
+ *
+ * @author user
+ */
+public class mainClass {
+    public static void main(String[] args) {
+        
+        tulip bt = new tulip();
+        bt.setCiri_ciri("343434");
+        bt.TampilData();
+        
+                
+    }
+   
+}
